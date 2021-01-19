59eaf10c7501306689008d1f23bb65be47228500
Juliet Garcia <51960035+Doggie28@users.noreply.github.com>
2021-01-18 22:35:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-<<<<<<< Updated upstream
-=======
+
 
 import discord
 
 
 class MyClient(discord.Client):
     async def on_ready(self):
-    print('Logged on as', self.user)
+        print('Logged on as', self.user)
 
     async def on_message(self, message):
         # don't respond to ourselves
@@ -16,4 +15,4 @@ class MyClient(discord.Client):
         if message.content == 'ping':
             await message.channel.send('pong')
 
->>>>>>> Stashed changes
+
